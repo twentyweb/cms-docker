1e2c0c176aff8a8f4809a4c00da48de4b814a824
Fabio Capucci <fabio.cappuc@gmail.com>
2021-06-15 10:59:55 +0200

    fix

--- web-app/scripts/entrypoint_laravel_queue.sh
+++ web-app/scripts/entrypoint_laravel_queue.sh
@@ -14,6 +14,6 @@ cd /app
 NAME=${NAME:-default}
 QUEUE=${QUEUE:-default}
 TRIES=${TRIES:-1}
-TIMEOUT=${TRIES:-1}
+TIMEOUT=${TIMEOUT:-1}
 
 php artisan queue:work --verbose --name=$NAME --queue=$QUEUE --tries=$TRIES --timeout=$TIMEOUT
