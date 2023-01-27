535624dfb8874a2dbed428d78772a560e069ba27
Fabio Capucci <fabio.cappuc@gmail.com>
2023-01-27 12:42:28 +0100

    add set -e to build scripts

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 if [ "$1" = 'backend' ]; then
     yarn install --non-interactive --silent
     yarn run backend:prod
--- composer/entrypoint.sh
+++ composer/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 isCommand() {
   for cmd in \
     "about" \
