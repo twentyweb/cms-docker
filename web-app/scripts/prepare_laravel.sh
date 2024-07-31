5840ef267adb0f5549867fcbc1ae6fa8c5ed7670
Fabio Capucci <fabio.cappuc@gmail.com>
2024-07-31 17:10:54 +0200

    updated unit config

--- web-app/scripts/prepare_laravel.sh
+++ web-app/scripts/prepare_laravel.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
-# Fix: telescope requires cache connection on registration
-export TELESCOPE_ENABLED=false
+cd /app
 
-mkdir -p storage/app/public && \
-mkdir -p storage/backups && \
-mkdir -p storage/framework/cache && \
-mkdir -p storage/framework/sessions && \
-mkdir -p storage/framework/views && \
+mkdir -p storage/app/public
+mkdir -p storage/backups
+mkdir -p storage/framework/cache
+mkdir -p storage/framework/sessions
+mkdir -p storage/framework/views
 mkdir -p storage/logs
 
-chown -R www-data:www-data . \
-&& chmod ug+wx -R storage bootstrap
+chown -R www-data:www-data .
+chmod ug+wx -R storage bootstrap
+
+# Fix: telescope requires cache connection on registration
+export TELESCOPE_ENABLED=false
 
 php artisan package:discover
--- web-app/scripts/runtime_cache.sh
+++ web-app/scripts/runtime_cache.sh
@@ -2,6 +2,8 @@
 
 cd /app
 
-php artisan config:cache || true
-php artisan route:cache || true
-php artisan view:cache || true
+if [ -f artisan ]; then
+    php artisan config:cache || true
+    php artisan route:cache || true
+    php artisan view:cache || true
+fi
