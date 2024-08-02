b427775ff26ed86e053b8ce143515caba0957c54
Fabio Capucci <fabio.cappuc@gmail.com>
2024-08-02 11:15:54 +0200

    moved runtime cache to start

--- web-app/scripts/entrypoint_laravel_app.sh
+++ web-app/scripts/entrypoint_laravel_app.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
+/scripts/runtime_cache.sh
+
 if [ $# -ne 0 ]; then
 	$@
-	exit 0;
+	exit 0
 fi
 
 set -e
-
-/scripts/runtime_cache.sh
 
 /init
--- web-app/scripts/runtime_cache.sh
+++ web-app/scripts/runtime_cache.sh
@@ -2,8 +2,9 @@
 
 cd /app
 
-if [ -f artisan ]; then
+if [ -f artisan ] && ["$DISABLE_CACHE" != "false"]; then
     php artisan config:cache || true
     php artisan route:cache || true
     php artisan view:cache || true
+    php artisan event:cache || true
 fi
